1e95913250eb98c935402e750401b6266cbe92f6
Dylan Price <the.dylan.price@gmail.com>
2013-03-23 18:36:24 -0700

    Started adding jsdoc annotations.

--- src/module.js
+++ src/module.js
@@ -4,11 +4,12 @@
  * Module for embedding Google Maps into AngularJS applications.
  *
  * See...
- * + directives/gmMap.js              for usage of the <gm-map> directive
- * + directives/gmMarkers.js          for usage of the <gm-markers> directive
- * + services/angulargmContainer.js  if you need to run custom configuration on the map, e.g. add new map types
  *
- * @module angulargm
+ * + [gmMap]{@link module:AngularGM/gmMap}  for usage of the <gm-map> directive
+ * + directives/gmMarkers.js                for usage of the <gm-markers> directive
+ * + services/angulargmContainer.js         if you need to run custom configuration on the map, e.g. add new map types
+ *
+ * @module AngularGM
  */
 (function() {
   angular.module('AngularGM', []).
