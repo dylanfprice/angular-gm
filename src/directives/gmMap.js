33607ae8e72731252965c6ffe04ad58e2104fb0e
Dylan Price <the.dylan.price@gmail.com>
2013-08-07 21:48:12 -0700

    fix links to angulargmContainer and angulargmDefaults.

--- src/directives/gmMap.js
+++ src/directives/gmMap.js
@@ -16,7 +16,7 @@
  * ignored.
  *
  * If you need to get a handle on the google.maps.Map object, see
- * [angulargmContainer]
+ * {@link angulargm.service:angulargmContainer angulargmContainer}
  *
  * @param {expression} gm-map-id angular expression that evaluates to a unique
  * string id for the map, e.g. `'map_canvas'` or `myMapId` where myMapId is a
@@ -42,7 +42,7 @@
  *
  * @param {expression} gm-map-options object in the current scope that is a
  * google.maps.MapOptions object. If unspecified, will use the values in
- * angulargmDefaults.mapOptions. [angulargmDefaults] is a service, so it is
+ * angulargmDefaults.mapOptions. {@link angulargm.service:angulargmDefaults angulargmDefaults} is a service, so it is
  * both injectable and overrideable (using $provide.decorator).
  *
  */
