4a058301335cccdfeb5803b86a955d224af28f14
Dylan Price <the.dylan.price@gmail.com>
2013-08-22 20:25:58 -0700

    Update to latest angular-gm release.

--- documentation/groups-metadata.js
+++ documentation/groups-metadata.js
@@ -1,3 +1,3 @@
 GROUP_DATA=[
-{"groupTitle":"AngularGM Docs","groupId":"angulargm","groupIcon":"icon-map-marker","sections":[
+{"groupTitle":"AngularGM Docs","groupId":"angulargm-0.2.0","groupIcon":"icon-map-marker","sections":[
 {"id":"api","title":"AngularGM API","scripts":["src/"],"doc_api":"angular"}],"visible":true}];
