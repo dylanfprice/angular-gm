45cc7fc9e6cb1f43b8ec147e466f4051990c6edf
Dylan Price <the.dylan.price@gmail.com>
2013-12-17 00:47:44 -0800

    Giving some organization and layout to examples.

--- js/angular-gm-example.js
+++ js/angular-gm-example.js
@@ -1,1 +1,26 @@
 angular.module('AngularGMExample', ['components', 'homepage'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/map/', {
+      templateUrl: 'partials/map.html',
+      active: 'map',
+    }).
+    when('/infowindows/', {
+      templateUrl: 'partials/infowindows.html',
+      active: 'infowindows',
+    }).
+    when('/markers/', {
+      templateUrl: 'partials/markers.html',
+      active: 'markers',
+    }).
+    when('/polylines/', {
+      templateUrl: 'partials/polylines.html',
+      active: 'polylines',
+    });
+}]).
+
+config(['$locationProvider', function($locationProvider) {
+  $locationProvider.html5Mode(false);
+}]);
+
