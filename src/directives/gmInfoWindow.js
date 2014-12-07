6a50a1981067aef028439c4171e4848783490f62
Dylan Price <the.dylan.price@gmail.com>
2014-12-06 17:14:07 -0800

    Don't  infowindow, instead hide it in the dom. Fixes #27

--- src/directives/gmInfoWindow.js
+++ src/directives/gmInfoWindow.js
@@ -64,19 +64,6 @@
           });
         });
       });
-
-      /* The info window's contents dont' need to be on the dom anymore,
-       google maps has them stored. So we just replace the infowindow
-       element with an empty div. (we don't just straight remove it from
-       the dom because straight removing things from the dom can mess up
-       angular) */
-      element.replaceWith('<div></div>');
-
-      //Decorate infoWindow.open to $compile contents before opening
-      var _open = infoWindow.open;
-      infoWindow.open = function open(map, anchor) {
-        _open.call(infoWindow, map, anchor);
-      };
     }
 
     return {
